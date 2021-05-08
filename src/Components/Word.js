4162b2f515c8b823d1a585ec4f77f78324df77b4
Navian <primeguyy@gmail.com>
2021-05-08 20:28:51 +0000

    update to css styles

--- src/Components/Word.js
+++ src/Components/Word.js
@@ -5,14 +5,25 @@ const Word = () => {
   const { selectedWord, correctLetters } = useGlobalContext();
 
   return (
-    <article className="word">
-      {selectedWord.split("").map((letter, index) => {
-        return (
-          <span key={index} className="letter">
-            {correctLetters.includes(letter) ? letter : ""}
-          </span>
-        );
-      })}
+    <article className="word-wrapper">
+      <div className="word">
+        {selectedWord.split("").map((letter, index) => {
+          return (
+            <span key={index} className="letter">
+              {correctLetters.includes(letter) ? letter : ""}
+            </span>
+          );
+        })}
+      </div>
+      <div className="smartphone__toggle-input">
+        <input
+          type="text"
+          className="toggle-input"
+          placeholder="tap here to activate keypad"
+          value=""
+          onChange={(e) => (e.target.value = "")}
+        />
+      </div>
     </article>
   );
 };
